a3e8d996f5fae15dbc2b43695515fa9fed956b3c
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-09 03:45:59 +0900

    test odbc

--- src/TestOcto.jl
+++ src/TestOcto.jl
@@ -1,23 +1,2 @@
 module TestOcto
-
-module Octo
-
-include("Deps.jl")
-include("types.jl")
-include("macros.jl")     # @sql_keywords @sql_functions
-include("Schema.jl")     # Schema.model Schema.changeset
-include("Queryable.jl")  # Queryable: from, as, window
-include("predicates.jl")
-include("Backends.jl")
-include("AdapterBase.jl")
-include("Pretty.jl")     # Pretty # Tablize Vector{<:NamedTuple}
-include("Repo.jl")       # Repo
-include("Adapters.jl")
-
 end
-
-import .Octo: Repo, Adapters
-
-end
-
-#@info :testocto TestOcto.Octo.Adapters.PostgreSQL
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,23 @@
 using Test
 
-ignores = [joinpath("adapters", "jdbc"),
-           joinpath("adapters", "odbc"),
-    ]
-#if Sys.iswindows()
-#end
+function runtests(tests)
+    n_passed = 0
+    anynonpass = 0
+    for (idx, filepath) in enumerate(all_tests)
+        numbering = string(idx, /, length(all_tests))
+        ts = @testset "$numbering $filepath" begin
+            include(filepath)
+        end
+        n_passed += ts.n_passed
+        anynonpass += ts.anynonpass
+    end
+    if iszero(anynonpass)
+        printstyled("✅  ", color=:green)
+        print("All ")
+        printstyled(n_passed, color=:green)
+        println(" tests have been completed.")
+    end
+end
 
 all_tests = []
 for (root, dirs, files) in walkdir(".")
@@ -13,14 +26,7 @@ for (root, dirs, files) in walkdir(".")
         "runtests.jl" == filename && continue
         filepath = joinpath(root, filename)[3:end]
         !isempty(ARGS) && !any(x->startswith(filepath, x), ARGS) && continue
-        isempty(ARGS) && any(path->occursin(path, filepath), ignores) && continue
         push!(all_tests, filepath)
     end
 end
-
-for (idx, filepath) in enumerate(all_tests)
-    numbering = string(idx, /, length(all_tests))
-    ts = @testset "$numbering $filepath" begin
-        include(filepath)
-    end
-end
+runtests(all_tests)
