2023d94a4a0345d07470acbcd031a52b8da9dca5
WooKyoung Noh <wookay.noh@gmail.com>
2020-09-19 18:27:53 +0900

    fix mysql test

--- test/adapters/mysql/execute_result_test.jl
+++ test/adapters/mysql/execute_result_test.jl
@@ -32,15 +32,15 @@ result = Repo.execute(Raw("""CREATE TABLE IF NOT EXISTS Employee
                  );"""))
 @test result === nothing
 
-Repo.execute(Raw("""INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno)
+result = Repo.execute(Raw("""INSERT INTO Employee (Name, Salary, JoinDate, LastLogin, LunchTime, OfficeNo, JobType, Senior, empno)
                  VALUES
                  ('John', 10000.50, '2015-8-3', '2015-9-5 12:31:30', '12:00:00', 1, 'HR', b'1', 1301),
                  ('Tom', 20000.25, '2015-8-4', '2015-10-12 13:12:14', '13:00:00', 12, 'HR', b'1', 1422),
                  ('Jim', 30000.00, '2015-6-2', '2015-9-5 10:05:10', '12:30:00', 45, 'Management', b'0', 1567);
               """))
+@test result.num_affected_rows == 3
 inserted = Repo.execute_result(INSERT)
 @test inserted.id == 1
-@test inserted.num_affected_rows == 3
 
 changes = (Name="Tim", Salary=15000.50, JoinDate="2015-7-25", LastLogin="2015-10-10 12:12:25", LunchTime="12:30:00", OfficeNo=56, JobType="Accounts", empno=3200)
 inserted = Repo.insert!(Employee, changes)
